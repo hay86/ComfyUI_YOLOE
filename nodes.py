4d84e9fff9c55e273808d8e44e1770ec91f00723
xukf <xukaifeng1986@gmail.com>
2025-04-19 21:43:40 +0000

    add json output

--- nodes.py
+++ nodes.py
@@ -1,6 +1,8 @@
 import os
 import torch
 import supervision as sv
+import numpy as np
+import json
 
 from ultralytics import YOLOE
 from torchvision.transforms.v2 import ToTensor, ToPILImage
@@ -147,16 +149,72 @@ class YOLOENode_Display:
         output = output.permute([0,2,3,1])
                 
         return (output[:, :, :, :3],)
+    
+
+class YOLOENode_Display_JSON:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                    'detections': ('DETECTIONS',),
+                    'max_conf_only': ('BOOLEAN', {'default': True}),
+                    'num_mask_samples': ('INT', {'default': 10, 'min': 1, 'max': 100, 'step': 1}),
+                     },}
+
+    CATEGORY = "YOLOENode"
+
+    RETURN_TYPES = ("STRING", )
+    RETURN_NAMES = ("JSON", )
+    FUNCTION = "display"
+
+
+    def display(self, detections, max_conf_only, num_mask_samples):
+        output = []
+
+        for detection in detections:
+            if max_conf_only:
+                max_per_class = {}  # class_id -> (idx, conf)
+                for idx, (cls_id, conf) in enumerate(zip(detection.class_id, detection.confidence)):
+                    if cls_id not in max_per_class or conf > max_per_class[cls_id][1]:
+                        max_per_class[cls_id] = (idx, conf)
+                top_conf_list = list(max_per_class.values())
+            else:
+                top_conf_list = [(idx, conf) for idx, conf in enumerate(detection.confidence)]
+
+            top_conf_list.sort(key=lambda x: x[1], reverse=True)
+
+            for idx, conf in top_conf_list:
+                cls_id = detection.class_id[idx]
+                cls_name = detection.data["class_name"][idx]
+                xyxy = detection.xyxy[idx].tolist()
+
+                mask = detection.mask[idx]
+                y, x = np.nonzero(mask)
+                sample_indices = np.random.choice(len(x), size=min(len(x), num_mask_samples), replace=False)
+                sampled_points = np.stack([x[sample_indices], y[sample_indices]], axis=1)
+                mask_samples = sampled_points.tolist()
+
+                output.append({
+                    "class_id": int(cls_id),
+                    "class_name": str(cls_name),
+                    "xyxy": xyxy,
+                    "mask_samples": mask_samples,
+                    "confidence": float(conf)
+                })
+            print(output)
+                
+        return (json.dumps(output),)
 
 
 NODE_CLASS_MAPPINGS = {
     "D_YOLOENode": YOLOENode,
     "D_YOLOENode_ModelLoader": YOLOENode_ModelLoader,
     "D_YOLOENode_Display": YOLOENode_Display,
+    "D_YOLOENode_Display_JSON": YOLOENode_Display_JSON,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "D_YOLOENode": "YOLOE Node",
     "D_YOLOENode_ModelLoader": "YOLOE Model Loader",
     "D_YOLOENode_Display": "YOLOE Display",
+    "D_YOLOENode_Display_JSON": "YOLOE Display JSON",
 }
